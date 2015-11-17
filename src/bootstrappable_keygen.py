feff85a94a2515ab23c03a3f01a398a99941668e
PMurph <murph.pm@gmail.com>
2015-11-17 14:30:51 -0600

    Added an initial implementation of the boostrappable keygen algorithm

---
+++ src/bootstrappable_keygen.py
@@ -0,0 +1,56 @@
+#!/bin/python
+
+import simple_error_distribution
+import somewhat_homomorphic_keygen
+import rvg
+
+class BootstrappableKeygen(object):
+    def __init__(self, short_dimension, long_dimension, multiplicative_depth, short_odd_modulus, long_odd_modulus, matrix_rows, short_seed=1, long_seed=1):
+        self.__short_dimension = short_dimension
+        self.__long_dimension = long_dimension
+        self.__multiplicative_depth = multiplicative_depth
+        self.__short_odd_modulus = short_odd_modulus
+        self.__long_odd_modulus = long_odd_modulus
+        
+        self.__short_error_distribution = simple_error_distribution.SimpleErrorDistribution(short_odd_modulus, short_seed)
+        self.__short_random_vector_generator = rvg.RVG(short_dimension, short_odd_modulus, short_seed)
+        
+        self.__sh_keygen = somewhat_homomorphic_keygen.SomewhatHomomorphicKeygen(long_dimension, multiplicative_depth, long_odd_modulus, matrix_rows, long_seed)
+        
+    def set_short_error_distribution(self, new_short_error_distribution):
+        self.__short_error_distribution = new_short_error_distribution
+        
+    def set_long_error_distribution(self, new_long_error_distribution):
+        self.__long_error_distribution = new_long_error_distribution
+        
+    def set_short_random_vector_generator(self, new_short_random_vector_generator):
+        self.__short_random_vector_generator = new_short_random_vector_generator
+        
+    def set_long_random_vector_generator(self, new_long_random_vector_generator):
+        self.__long_random_vector_generator = new_long_random_vector_generator
+        
+    def generate_keys(self):
+        secret_key, eval_key, public_key = self.__sh_keygen.generate_keys()
+        
+        short_secret_key, short_eval_key = self.__generate_short_keys(secret_key)
+        
+        return short_secret_key, (eval_key, short_eval_key), public_key
+        
+    def __generate_short_keys(self, secret_key):
+        short_secret_key = self.__short_random_vector_generator.generate()
+        short_eval_key = []
+        for i in range(self.__long_dimension):
+            tau = 0
+            for coefficient_vector in self.__short_random_vector_generator.generate(int(math.log(self.__long_odd_modulus, 2)) + 1):
+                error = self.__short_error_distribution.sample_distribution()
+                b = (coefficient_vector.dot(short_secret_key) + error + int(math.round(float(self.__short_odd_modulus)/self.__long_odd_modulus) * s**tau * secret_key[i])) % self.__short_odd_modulus
+                
+                short_eval_key.append((coefficient_vector.tolist(), b))
+                
+                tau += 1
+       
+       return short_secret_key, short_eval_key
+            
+
+if __name__=="__main__":
+    print "BootstrappableKeygen"

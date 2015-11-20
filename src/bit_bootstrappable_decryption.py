90f06d4803819681a3a296b5c6a04c28ec399287
PMurph <murph.pm@gmail.com>
2015-11-20 09:25:53 -0600

    Added Decryption Algorithm for a single bit

---
+++ src/bit_bootstrappable_decryption.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+class BitBootstrappableDecryption(object):
+    def __init__(self, short_odd_modulus):
+        self.__short_odd_modulus = short_odd_modulus
+        
+    def decrypt(self, ciphertext, secret_key):
+        return ((ciphertext[1] - ciphertext[0].dot(secret_key)) % self.short_odd_modulus) % 2
+
+if __name__=="__main__":
+    print "BitBootstrappableDecryption class"

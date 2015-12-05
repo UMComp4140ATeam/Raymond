75aff8a1f68d8e0320268f1385eba1b54010db1f
chrwc <Christopher W. Catton>
2015-12-05 15:10:13 -0600

    working on evaluate... example construction of circut inside evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,89 +1,33 @@
 import homomorphic_arithmetic
 
-def evalAdd(ctexts):
-    if not isinstance(ctexts, list):
-        return None
-    v = None
-    w = None
-    l = None
-
-    return None
-
-def evalMult(ctexts):
-    if not isinstance(ctexts, list):
-        return None
-    if len(ctexts) != 2:
-        return None
-
-
-    return None
-
-class Gate:
-    def __init__(self, inputs, addOrMult):
-        self._inputs = inputs
-        self._addOrMult = addOrMult & 1
-
-    def setInput(self, inputs):
-        self._inputs = inputs
-
-    def process(self, ctexts):
-        c = []
-        for i in range(0, len(self._inputs)):
-            if i == 1:
-                c.append(ctexts[i])
 class Layer:
-    def __init__(self, sizei=1, addOrMult):
-        self.addOrMult = addOrMult & 1 #1 for multiply, 0 for Add
-        self.size = 0
-        if size > 0:
-            self.size = size
+    def __init__(self, gates=[], addOrMult):
+        self._addOrMult = addOrMult & 1 #1 for multiply, 0 for Add
 
-        self.gates = []*self.size
+        self._gates = gates
 
-    def setInput(self, inputs):
-        for i in inputs:
-            self.gates.setInput(i)
+    def process(self, dimension, ctexts):
+        for g in self._gates:
+            ntexts = []
+            for c in g:
+                ntexts.append(ctexts[c])
 
-    def process(self, ctexts):
-        for i in range(0, self.size):
-            "process"
+            if self._addOrMult == 0:
+                homomorphic_arithmetic.homorphic_add(dimension, ntexts)
+            elif self._addOrMult == 1:
+                homomorphic_arithmetic.homorphic_mult(dimension, ntexts)
 
 class Circuit:
     def __init__(self, layers=[]):
         self._layers = layers
 
-    def process(self, ctexts):
+    def process(self, dimension, ctexts):
         for l in self._layers:
-            l.process(ctexts)
-
-def evalLayer(layer, ctexts):
-    ntexts = ctexts
-    if layer.addOrMult == 0:
-        for i in range(0, layer.size):
-            ntexts = evalAdd(ctexts)
-    elif layer.addOrMult == 1:
-        for i in range(0, layer.size):
-            ntexts = evalMult(ctexts)
-
-    return ntexts
-
-def getCipherTextsByLayer(ctexts, layer):
-    ret = []
-    for c in ctexts:
-        if c.layer() == layer:
-            ret.append(c)
-    return ret
+            l.process(dimension, ctexts)
 
 def eval(dimension, f, ctexts):
     "f is a circuit, c is an array of ciphertexts"
-    ntexts = ctexts
-    start = 0
-    end = 0
-    for layer in f.layers:
-        end = start + layer.size
-        ntexts0 = evalLayer(layer, ntexts[start:end])
-
-    return None
+    return f.process(dimension, ctexts)
 
 ###Example Construction###
 

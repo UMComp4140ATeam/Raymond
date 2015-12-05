c58a0b48a7f190ab2407376e2ea41ab0e3b54557
chrwc <Christopher W. Catton>
2015-12-05 15:02:45 -0600

    working on evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,5 @@
+import homomorphic_arithmetic
+
 def evalAdd(ctexts):
     if not isinstance(ctexts, list):
         return None
@@ -16,20 +18,43 @@ def evalMult(ctexts):
 
     return None
 
+class Gate:
+    def __init__(self, inputs, addOrMult):
+        self._inputs = inputs
+        self._addOrMult = addOrMult & 1
+
+    def setInput(self, inputs):
+        self._inputs = inputs
+
+    def process(self, ctexts):
+        c = []
+        for i in range(0, len(self._inputs)):
+            if i == 1:
+                c.append(ctexts[i])
 class Layer:
-    def __init__(self, size, addOrMult):
+    def __init__(self, sizei=1, addOrMult):
         self.addOrMult = addOrMult & 1 #1 for multiply, 0 for Add
         self.size = 0
         if size > 0:
             self.size = size
-        self.fanin = 2
 
-        if self.addOrMult == 0:
-            self.fanin = -1
+        self.gates = []*self.size
+
+    def setInput(self, inputs):
+        for i in inputs:
+            self.gates.setInput(i)
+
+    def process(self, ctexts):
+        for i in range(0, self.size):
+            "process"
 
 class Circuit:
-    def __init__(self):
-        self.layers = []
+    def __init__(self, layers=[]):
+        self._layers = layers
+
+    def process(self, ctexts):
+        for l in self._layers:
+            l.process(ctexts)
 
 def evalLayer(layer, ctexts):
     ntexts = ctexts
@@ -42,10 +67,39 @@ def evalLayer(layer, ctexts):
 
     return ntexts
 
-def eval(f, ctexts):
+def getCipherTextsByLayer(ctexts, layer):
+    ret = []
+    for c in ctexts:
+        if c.layer() == layer:
+            ret.append(c)
+    return ret
+
+def eval(dimension, f, ctexts):
     "f is a circuit, c is an array of ciphertexts"
     ntexts = ctexts
+    start = 0
+    end = 0
     for layer in f.layers:
-        ntexts = evalLayer(layer, ntexts)
+        end = start + layer.size
+        ntexts0 = evalLayer(layer, ntexts[start:end])
 
     return None
+
+###Example Construction###
+
+#Construct Layer(s)#
+g0 = [0, 1]
+g1 = [1, 2]
+g2 = [2, 3, 4]
+l0 = Layer([g0,g1,g2], 0)
+
+g3 = [5, 1]
+g4 = [6, 7]
+l1 = Layer([g3, g4], 1) #mult
+
+g5 = [8, 9]
+l2 = Layer([g5], 0)
+
+Circuit([l0, l1, l2])
+#Construct
+
--- tst/homomorphic_arithmetic_test.py
+++ tst/homomorphic_arithmetic_test.py
@@ -18,26 +18,26 @@ class HomomorphicArithmeticTest(unittest.TestCase):
 
     def test_homomorphic_add_empty_list(self):
         self.assertIsNone(homomorphic_arithmetic.homomorphic_add(self.dimension, []))
-        
+
     def test_homomorphic_add_single_ciphertext(self):
         resulting_ciphertext = homomorphic_arithmetic.homomorphic_add(self.dimension, [self.ciphertexts[0]])
-        
+
         self.assertEquals(self.ciphertexts[0].coefficient_vector.tolist(), resulting_ciphertext.coefficient_vector.tolist())
         self.assertEquals(self.ciphertexts[0].ciphertext, resulting_ciphertext.ciphertext)
         self.assertEquals(self.ciphertexts[0].level, resulting_ciphertext.level)
-        
+
     def test_homomorphic_add_multiple_ciphertexts(self):
         resulting_ciphertext = homomorphic_arithmetic.homomorphic_add(self.dimension, self.ciphertexts)
-        
+
         self.assertEquals([5, 5, 5], resulting_ciphertext.coefficient_vector.tolist())
         self.assertEquals(4, resulting_ciphertext.ciphertext)
         self.assertEquals(0, resulting_ciphertext.level)
-        
+
     def test_homomorphic_add_different_levels(self):
         self.assertRaises(ValueError, homomorphic_arithmetic.homomorphic_add, 3, [self.ciphertexts[0], ciphertext.Ciphertext(numpy.array([1, 1, 1], dtype=numpy.integer), 1, 1)])
-        
+
     def test_homomorphic_add_different_dimensions(self):
         self.assertRaises(ValueError, homomorphic_arithmetic.homomorphic_add, 3, [ciphertext.Ciphertext(numpy.array([1, 1, 2, 1], dtype=numpy.integer), 1, 0)])
-        
+
 if __name__=="__main__":
     print "HomomorphicArithmetic Tests"

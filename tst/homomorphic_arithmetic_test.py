836f632125b0546b4f1bcd122b057bca91bb6716
Patrick Murphy <murph.pm@gmail.com>
2015-12-06 16:25:40 -0600

    Changed homomorphic arithmetic tests to use new homomorphic arithmetic class

--- tst/homomorphic_arithmetic_test.py
+++ tst/homomorphic_arithmetic_test.py
@@ -10,6 +10,7 @@ class HomomorphicArithmeticTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dimension = 3
+        cls.odd_modulus = 5
         cls.ciphertexts = [
             ciphertext.Ciphertext(numpy.array([1, 2, 3], dtype=numpy.integer), 1, 0),
             ciphertext.Ciphertext(numpy.array([3, 2, 1], dtype=numpy.integer), 2, 0),
@@ -17,27 +18,32 @@ class HomomorphicArithmeticTest(unittest.TestCase):
         ]
 
     def test_homomorphic_add_empty_list(self):
-        self.assertIsNone(homomorphic_arithmetic.homomorphic_add(self.dimension, []))
+        arithmetic = homomorphic_arithmetic.HomomorphicArithmetic(self.dimension, self.odd_modulus, {})
+        self.assertIsNone(arithmetic.homomorphic_add([]))
         
     def test_homomorphic_add_single_ciphertext(self):
-        resulting_ciphertext = homomorphic_arithmetic.homomorphic_add(self.dimension, [self.ciphertexts[0]])
+        arithmetic = homomorphic_arithmetic.HomomorphicArithmetic(self.dimension, self.odd_modulus, {})
+        resulting_ciphertext = arithmetic.homomorphic_add([self.ciphertexts[0]])
         
         self.assertEquals(self.ciphertexts[0].coefficient_vector.tolist(), resulting_ciphertext.coefficient_vector.tolist())
         self.assertEquals(self.ciphertexts[0].ciphertext, resulting_ciphertext.ciphertext)
         self.assertEquals(self.ciphertexts[0].level, resulting_ciphertext.level)
         
     def test_homomorphic_add_multiple_ciphertexts(self):
-        resulting_ciphertext = homomorphic_arithmetic.homomorphic_add(self.dimension, self.ciphertexts)
+        arithmetic = homomorphic_arithmetic.HomomorphicArithmetic(self.dimension, self.odd_modulus, {})
+        resulting_ciphertext = arithmetic.homomorphic_add(self.ciphertexts)
         
         self.assertEquals([5, 5, 5], resulting_ciphertext.coefficient_vector.tolist())
         self.assertEquals(4, resulting_ciphertext.ciphertext)
         self.assertEquals(0, resulting_ciphertext.level)
         
     def test_homomorphic_add_different_levels(self):
-        self.assertRaises(ValueError, homomorphic_arithmetic.homomorphic_add, 3, [self.ciphertexts[0], ciphertext.Ciphertext(numpy.array([1, 1, 1], dtype=numpy.integer), 1, 1)])
+        arithmetic = homomorphic_arithmetic.HomomorphicArithmetic(self.dimension, self.odd_modulus, {})
+        self.assertRaises(ValueError, arithmetic.homomorphic_add, [self.ciphertexts[0], ciphertext.Ciphertext(numpy.array([1, 1, 1], dtype=numpy.integer), 1, 1)])
         
     def test_homomorphic_add_different_dimensions(self):
-        self.assertRaises(ValueError, homomorphic_arithmetic.homomorphic_add, 3, [ciphertext.Ciphertext(numpy.array([1, 1, 2, 1], dtype=numpy.integer), 1, 0)])
+        arithmetic = homomorphic_arithmetic.HomomorphicArithmetic(self.dimension, self.odd_modulus, {})
+        self.assertRaises(ValueError, arithmetic.homomorphic_add, [ciphertext.Ciphertext(numpy.array([1, 1, 2, 1], dtype=numpy.integer), 1, 0)])
         
 if __name__=="__main__":
     print "HomomorphicArithmetic Tests"
